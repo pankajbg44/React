3f83a1c98af501852be253a6e5c889fe34d008f4
pankajbg44 <pankajbg44@gmail.com>
2023-05-22 16:30:32 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 function App() {
-  let heading = "Hello World!!!";
+  let text = "Hello Lorem!!!";
+  let para ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta esse, fugiat labore, reprehenderit facere perspiciatis voluptate, ducimus et aut ratione inventore? At rem quas fugiat soluta! Cumque minima quas ad sapiente quae consequuntur accusamus aut nam beatae dignissimos veritatis, eum vel quidem iure praesentium reiciendis possimus enim aperiam excepturi dolore.";
+  let style = "bg-light text-primary p-3 h4 shadow p-2 mb-2";
+  let style2 = "text-bg-warning d-flex justify-content-center shadow p-2 mb-3"
 
   return (
     <div>
-      <h1>{heading}</h1>
+      <h1 className={style2}>{text}</h1>
+      <p className={style}>{para}</p>
+      <p className={style}>{para}</p>
+      <p className={style}>{para}</p>
     </div>
   );
 }
