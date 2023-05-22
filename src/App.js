1e6e25c47acfb048322d7c2019724930d73aa1ff
pankajbg44 <pankajbg44@gmail.com>
2023-05-22 17:08:25 +0530

    b5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 function App() {
   let text = "Hello Lorem!!!";
   let para ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta esse, fugiat labore, reprehenderit facere perspiciatis voluptate, ducimus et aut ratione inventore? At rem quas fugiat soluta! Cumque minima quas ad sapiente quae consequuntur accusamus aut nam beatae dignissimos veritatis, eum vel quidem iure praesentium reiciendis possimus enim aperiam excepturi dolore.";
-  let style = "bg-light text-primary p-3 h4 shadow p-2 mb-2";
-  let style2 = "text-bg-warning d-flex justify-content-center shadow p-2 mb-3"
+  let style1 = {color:"maroon", backgroundColor: "rgb(178, 214, 243)",padding:8, textAlign:"center"};
+  let style2 = {color:"maroon", backgroundColor: "rgb(193, 240, 165)",padding:8, margin:3, fontSize:20};
 
   return (
     <div>
-      <h1 className={style2}>{text}</h1>
-      <p className={style}>{para}</p>
-      <p className={style}>{para}</p>
-      <p className={style}>{para}</p>
+      <h1 style={style1}>{text}</h1>
+      <p style={style2}>{para}</p>
+      <p style={style2}>{para}</p>
     </div>
   );
 }
