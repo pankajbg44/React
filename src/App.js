d5d08a42e8a56594dd9beec0b2a1ae5dfe3fdc85
pankajbg44 <pankajbg44@gmail.com>
2023-05-22 13:15:02 +0530

    react day1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./Pages/Footer";
+import MainBody from "./Pages/MainBody";
+import Header from "./Pages/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <Footer />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/Pages/MainBody.js
@@ -0,0 +1,14 @@
+function MainBody() {
+    return (
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          porro hic saepe. Aperiam repellat culpa soluta assumenda molestias! Quis
+          voluptates atque rem libero labore voluptatibus nostrum nesciunt.
+          Veritatis, dolorum voluptate.
+        </p>
+      </div>
+    );
+  }
+  
+  export default MainBody;
